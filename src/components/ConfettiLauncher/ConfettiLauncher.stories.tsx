89a6603ca9ba74d1c2552816b9561724890fa74b
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2024-09-14 22:08:20 -0700

    chore: setup Storybook and add initial configuration

---
+++ src/components/ConfettiLauncher/ConfettiLauncher.stories.tsx
@@ -0,0 +1,8 @@
+import ConfettiLauncher from './ConfettiLauncher';
+
+export default {
+  title: 'Components/ConfettiLauncher', // Storybook上での表示名
+  component: ConfettiLauncher,
+};
+
+export const Default = () => <ConfettiLauncher />;
